fe61bcf2d6d327792e80a32af572c60b5fe899b2
LiuPeng <LiuPeng@mallcoo.cn>
2016-05-25 13:38:37 +0800

    制作通用库

---
+++ TestUniversalFramework/TestUniversalFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestUniversalFramework.h
+//  TestUniversalFramework
+//
+//  Created by admin on 16/5/25.
+//  Copyright © 2016年 LiuPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestUniversalFramework.
+FOUNDATION_EXPORT double TestUniversalFrameworkVersionNumber;
+
+//! Project version string for TestUniversalFramework.
+FOUNDATION_EXPORT const unsigned char TestUniversalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestUniversalFramework/PublicHeader.h>
+
+
